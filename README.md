61325c0cea1c49c9832a698b70e127476b2c3386
Ryan Freese <ryan.p.freese@gamil.com>
2020-10-14 19:37:34 -0500

    Uploading scripts

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# README_generator
-This node.js program will aid the user in creation of a professional README.
+# readmeGenerator
+
+node readme generator
+
+## Contact Info
+
+john.doe@gmail.com
+
+KeepTheLidOnTight
+
+## Contributing
+
+
+
+##Installation
+
+npm i
+
+## Tests
+To run tests, run the following command:
+
+npm test
+
+## Questions
+
+
+
+## License
+
+MIT
+
+If you have any questions about the repo, open an issue or contact me directly at john.doe@gmail.com. You can find more of my work at [KeepTheLidOnTight](https://github.com/KeepTheLidOnTight/).
+
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,39 @@
+function generateMarkdown(data) {
+
+return `# ${data.title}
+
+${data.description}
+
+## Contact Info
+
+${data.email}
+
+${data.github}
+
+## Contributing
+
+${data.contributing}
+
+##Installation
+
+${data.installation}
+
+## Tests
+To run tests, run the following command:
+
+${data.test}
+
+## Questions
+
+${data.usage}
+
+## License
+
+${data.license}
+
+If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.github}](https://github.com/${data.github}/).
+
+`;
+}
+
+module.exports = generateMarkdown;
